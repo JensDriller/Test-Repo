d29bf1f08521c42e42e31c3a888698960e0b5bd9
Jens Driller <jens@jensdriller.com>
2015-08-08 13:58:00 +0100

    Fix global env vars

--- scripts/deploy_snapshot.sh
+++ scripts/deploy_snapshot.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo "Deploying snapshot"
-
 mvn clean deploy --settings="scripts/settings.xml"
-#mvn clean source:jar javadoc:jar deploy --settings=".buildscript/settings.xml" -Dmaven.test.skip=true
-
-echo "Snapshot deployed"
